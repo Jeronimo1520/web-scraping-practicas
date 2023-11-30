f06cb9d79f2dadb1256c866245cee39faf0efc04
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-11-29 19:13:08 -0500

    Cambios scrapy diario EL Universo

--- PaginasEstaticas/diarioElUniversoScrapy.py
+++ PaginasEstaticas/diarioElUniversoScrapy.py
@@ -4,37 +4,43 @@ from scrapy.spiders import Spider
 from bs4 import BeautifulSoup
 from scrapy.selector import Selector
 from scrapy.loader import ItemLoader
+
 """
 OBJETIVO: 
     - Extraer las noticias de la seccion de deportes de Diario El Universo"""
+
+
 class Article(Item):
     id = Field()
     title = Field()
     description = Field()
     category = Field()
 
+
 class ElUniversoSpider(Spider):
     name = "ElUniversoSpider"
     custom_settings = {
-        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu '
+                      'Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
     }
 
     start_urls = ['https://www.eluniverso.com/deportes/']
 
     def parse(self, response):
-
         sel = Selector(response)
-        articles = sel.xpath('(//ul[@class="feed | divide-y relative  "])[2]/li') #Mi iterable
+        articles = sel.xpath('(//ul[@class="feed | divide-y relative  "])[2]/li')  # Mi iterable
         for i, article in enumerate(articles):
             item = ItemLoader(Article(), article)
             item.add_xpath('title', './/h2/a/text()')
             item.add_xpath('description', './/p/text()')
-            item.add_xpath('category', './/a[@class="primary_section | font-secondary text-silver-500 dark:text-silver-200 uppercase"]/text()')
+            item.add_xpath('category',
+                           './/a[@class="primary_section | font-secondary text-silver-500 dark:text-silver-200 '
+                           'uppercase"]/text()')
             item.add_value('id', i)
 
             yield item.load_item()
-        #IMPLEMENTACION CON BS4 JUNTO A SCRAPY, AUMENTA LA COMPLEJIDAD:
-        #ME TRAE SECCION DE COLUMNISTAS Y ALGUNAS REPETIDAS, datos en dataElUniverso2.json
+        # IMPLEMENTACION CON BS4 JUNTO A SCRAPY, AUMENTA LA COMPLEJIDAD:
+        # ME TRAE SECCION DE COLUMNISTAS Y ALGUNAS REPETIDAS, datos en dataElUniverso2.json
         #
         # soup = BeautifulSoup(response.body, 'html.parser')
         # articles_container = soup.find_all(class_='feed | divide-y relative')
@@ -51,8 +57,16 @@ class ElUniversoSpider(Spider):
         #             item.add_value('description', 'N/A')
         #         category = article.find('a', class_='primary_section | font-secondary text-silver-500 dark:text-silver-200 uppercase').text
         #         item.add_value('title', title)
-        #         item.add_value('description', description)
+        #
         #         item.add_value('category', category)
         #         id +=1
         #         item.add_value('id', id)
         #         yield item.load_item()
+
+        # CORRIENDO SCRAPY SIN LA TERMINAL
+        # process = CrawlerProcess({
+        #     'FEED_FORMAT': 'json',
+        #     'FEED_URI': 'datos_de_salida.json'
+        # })
+        # process.crawl(ElUniversoSpider)
+        # process.start()

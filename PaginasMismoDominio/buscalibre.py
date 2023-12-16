55a256402116be6ba6a358e5967c2976cd358180
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-12-16 15:34:10 -0500

    Manejo de datos nulos en scraping buscalibre

--- PaginasMismoDominio/buscalibre.py
+++ PaginasMismoDominio/buscalibre.py
@@ -25,7 +25,7 @@ class BuscalibreCrawler(CrawlSpider):
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/119.0.0.0 Safari/537.36',
         'FEED_EXPORT_ENCODING': 'utf-8',
-        'CLOSESPIDER_PAGECOUNT': 200
+        'CLOSESPIDER_PAGECOUNT': 50
     }
 
     download_delay = 2
@@ -49,21 +49,32 @@ class BuscalibreCrawler(CrawlSpider):
         nuevo_texto = texto.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').strip()
         return nuevo_texto
 
+    def quitar_porcentaje(self, texto):
+        return texto.replace("%", "")
+
     def parse_libros(self, response):
         item = ItemLoader(Libro(), response)
         item.add_xpath('nombre', '//p[@class="tituloProducto"]/text()')
+
         sel = Selector(response)
         autores = sel.xpath('//div[@id="metadata-autor"]/a[@class="color-primary font-weight-medium '
                             'link-underline"]/text()').getall()
+
         item.add_value('autor', [self.limpiar_texto(autor) for autor in autores])
         item.add_xpath('precioActual','//p[contains(@class,"precioAhora")]/span/text()')
+
         precio_antes = sel.xpath('//p[contains(@class,"precioAntes")]/text()').get()
         if precio_antes:
             item.add_value('precioAntes', precio_antes, MapCompose(self.limpiar_texto))
         else:
             item.add_value('precioAntes', 'N/A')
+
         item.add_xpath('editorial', '//div[@id="metadata-editorial"]/a/text()')
-        item.add_xpath('descuento','//div[contains(@class,"box-descuento font")]/strong/text()')
+        descuento = sel.xpath('//div[contains(@class,"box-descuento font")]/strong/text()').get()
+        if descuento:
+            item.add_value('descuento', descuento, MapCompose(self.quitar_porcentaje))
+        else:
+            item.add_value('descuento','N/A')
         yield item.load_item()
 
 

07ae23297e0020db26f500766593b34b4907468f
Jerónimo Bedoya B <jeronimobedoya1520@gmail.com>
2023-12-31 14:33:23 -0500

    Uso de MongoDB para almecenamiento de datos

---
+++ Extras/olx_mongo.py
@@ -0,0 +1,74 @@
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+password = open('password_mongo.txt').readline().strip()
+uri = f"mongodb+srv://Jeronimo1520:{password}@cluster0.whzzoqw.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client['olx']
+col = db['anuncios_selenium']
+'''
+OBJETIVO: 
+   -Almacenar datos en mongodb
+    '''
+
+options = Options()
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.3")
+options.page_load_strategy = 'normal'
+
+# Instancio el driver de selenium que va a controlar el navegador
+# A partir de este objeto voy a realizar el web scraping e interacciones
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://www.olx.in')
+sleep(2)
+driver.refresh()  # Solucion de un bug extraño en Windows en donde los anuncios solo cargan al hacerle refresh a la página
+sleep(2)
+
+# Busco el boton para cargar mas informacion
+
+for i in range(3):
+    try:
+        # Esperamos a que el boton se encuentre disponible a traves de una espera por eventos
+        # Espero un maximo de 10 segundos, hasta que se encuentre el boton dentro del DOM
+        boton = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//button[@data-aut-id="btnLoadMore"]')
+                                           ))
+        boton.click()
+        nAnuncios = 20 + ((i + 1) * 20)  # 20 anuncios de carga inicial, y luego 20 anuncios por cada click que he dado
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.XPATH, '//li[@data-aut-id="itemBox"]//span[@data-aut-id="itemPrice"]'))
+        )
+    except:
+        break
+
+# Encuentro cual es el XPATH de cada elemento donde esta la informacion que quiero extraer
+# Esto es una LISTA. Por eso el metodo esta en plural
+
+anuncios = driver.find_elements(By.XPATH, '//li[@data-aut-id="itemBox"]')
+
+for anuncio in anuncios:
+    try:
+        precio = anuncio.find_element(By.XPATH, './/span[@data-aut-id="itemPrice"]').text
+    except:
+        print("El anuncio carece de precio")
+
+    try:
+        titulo = anuncio.find_element(By.XPATH, './/span[@data-aut-id="itemTitle"]').text
+
+    except:
+        print('Anuncio carece de titulo ')
+
+    col.insert_one({
+        'precio': precio,
+        'titulo': titulo
+    })
+
